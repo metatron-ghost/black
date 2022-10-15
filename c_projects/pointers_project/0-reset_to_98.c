56a06d7d08e49b54d0e0730348a2eae95bbdf0ce
metatron-ghost <arabzoh90@gmail.com>
2022-10-15 13:55:30 +0200

    First pointer done

---
+++ c_projects/pointers_project/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that changes a variable value
+ *
+ * @n: the pointer to the variable received from main
+ *
+ * Return: void (Nothingg)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
