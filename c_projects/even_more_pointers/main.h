60cfe8c42f8527323b0523c564bb70ae1968b200
Arab Metatron <112793611+metatron-ghost@users.noreply.github.com>
2022-11-02 12:26:45 +0200

    Update main.h

--- c_projects/even_more_pointers/main.h
+++ c_projects/even_more_pointers/main.h
@@ -6,5 +6,7 @@
 char *_memset(char *s, char b, unsigned int n);
 char *_strchr(char *s, char c);
 char *_strpbrk(char *s, char *accept);
+void print_chessboard(char (*a)[8]);
+int _putchar(char c);
 
 #endif /* MAIN_H */
