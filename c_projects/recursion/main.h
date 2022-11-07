f1775855a6b3d7cb00c3dca6f0444ba539b35811
Arab Metatron <112793611+metatron-ghost@users.noreply.github.com>
2022-11-07 10:31:52 +0200

    Update main.h

--- c_projects/recursion/main.h
+++ c_projects/recursion/main.h
@@ -7,5 +7,7 @@ int _putchar(char c);
 void _puts_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
+int is_palindrome(char *s);
+int check(char *s, int j, int m);
 
 #endif /* MAIN_H */
