f9bc5247b7e3ca36d9a0268ee81d0d39c9e03527
metatron-ghost <arabzoh90@gmail.com>
2022-11-17 09:46:46 +0200

    Minimum coins Task Done

--- c_projects/argc_argv/100-change.c
+++ c_projects/argc_argv/100-change.c
@@ -24,38 +24,30 @@ int main(int argc, char *argv[])
 	else
 	{
 		amount = atoi(argv[1]);
-		if (amount <= 0)
+		while (amount >= 25)
 		{
-			printf("%d\n", count);
-			return (0);
+			count++;
+			amount -= 25;
 		}
-		else
+		while (amount >= 10)
 		{
-			while (amount >= 25)
-			{
-				count++;
-				amount -= 25;
-			}
-			while (amount >= 10)
-			{
-				count++;
-				amount -= 10;
-			}
-			while (amount >= 5)
-			{
-				count++;
-				amount -= 5;
-			}
-			while (amount >= 2)
-			{
-				count++;
-				amount -= 2;
-			}
-			while (amount >= 1)
-			{
-				count++;
-				amount -= 1;
-			}
+			count++;
+			amount -= 10;
+		}
+		while (amount >= 5)
+		{
+			count++;
+			amount -= 5;
+		}
+		while (amount >= 2)
+		{
+			count++;
+			amount -= 2;
+		}
+		while (amount >= 1)
+		{
+			count++;
+			amount -= 1;
 		}
 	}
 	printf("%d\n", count);
