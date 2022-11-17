c1ac8d86dde7695da02f15aff63638efdf097ef8
metatron-ghost <arabzoh90@gmail.com>
2022-11-17 09:36:10 +0200

    Minimum coins Task Done

---
+++ c_projects/argc_argv/100-change.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - entry point
+ *
+ * Description: A program that calculates the minimum coins required for change
+ * @argc: Number of arguments given (including program name)
+ * @argv: pointer to the strings of arguments given
+ *
+ * Return: 0 on success and 1 on failure
+ */
+
+int main(int argc, char *argv[])
+{
+	int count = 0, amount;
+
+	if (argc != 2)
+	{
+		printf("Error.\n");
+		return (1);
+	}
+	else
+	{
+		amount = atoi(argv[1]);
+		if (amount <= 0)
+		{
+			printf("%d\n", count);
+			return (0);
+		}
+		else
+		{
+			while (amount >= 25)
+			{
+				count++;
+				amount -= 25;
+			}
+			while (amount >= 10)
+			{
+				count++;
+				amount -= 10;
+			}
+			while (amount >= 5)
+			{
+				count++;
+				amount -= 5;
+			}
+			while (amount >= 2)
+			{
+				count++;
+				amount -= 2;
+			}
+			while (amount >= 1)
+			{
+				count++;
+				amount -= 1;
+			}
+		}
+	}
+	printf("%d\n", count);
+	return (0);
+}
