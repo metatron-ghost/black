2edae2abc4041be9bae3787ec03274275f02f5e8
metatron-ghost <arabzoh90@gmail.com>
2022-11-24 10:39:32 +0200

    Function to initialise

---
+++ c_projects/structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+
+		/* or use (*d).owner = owner format*/
+	}
+}
