cb13bbf82c456788aae8291218c17c4b9aee3576
metatron-ghost <arabzoh90@gmail.com>
2022-12-13 16:15:04 +0200

    Some more snake

---
+++ python_projects/python-hello_world/7-edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+word = "Holberton"
+word_first_3, word_last_2, middle_word = word[:3], word[7:9], word[1:8]
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
---
+++ python_projects/python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[-22:-17] + str[:6]
+print(str)
